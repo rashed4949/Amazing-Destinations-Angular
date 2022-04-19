7c76438a07ae8becb3bf51d7e4842bc624ec8ca2
rashed4949 <rrashed4@gmail.com>
2022-04-19 03:01:04 +0200

    firebase solve

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDRYHxJteuWb1gmbv5WOrOPQ8vdEa7mCk4",
+    authDomain: "amazing-destinations-e83ed.firebaseapp.com",
+    projectId: "amazing-destinations-e83ed",
+    storageBucket: "amazing-destinations-e83ed.appspot.com",
+    messagingSenderId: "871135385901",
+    appId: "1:871135385901:web:4d94c7f9cf5d10c90b5ba4",
+    measurementId: "G-3399D0SDRW"
+  }
 };
